55cb1074790c4efc501ce756ffd85d6ce7435523
George <gwwebster23@gmail.com>
2023-02-27 18:07:48 -0600

    Scope lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Write your solution in this file!
+var customerName = 'bob'
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase()
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+}
+
+const leastFavoriteCustomer = 'Definitely not George'
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "It's definitely Bob"
+}
